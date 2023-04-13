2c23570c38f2df40722087ca7c59b7c4dd4325f6
Yang Cui <123989119+yangcui23@users.noreply.github.com>
2023-04-13 13:46:58 -0600

    Delete product_manager directory

--- product_manager/server/routes/product.route.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const ProductController = require("../controllers/product.controller");
-
-
-module.exports = function(app){
-    app.get('/api/product', ProductController.findAllProducts)
-    app.post('/api/product/new', ProductController.createNewProduct);
-}
